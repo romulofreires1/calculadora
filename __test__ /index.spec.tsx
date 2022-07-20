837874b5630e0c9d581d8aeee768bf08dcba9c93
Francisco Romulo Freires do Nascimento <romulofreires1@gmail.com>
2022-07-20 07:57:27 -0300

    configuração de teste

---
+++ __test__ /index.spec.tsx
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+import { calculate } from "../pages/index";
+
+test("caculation function", async () => {
+  expect(calculate(5, 1, 2)).toBe(7);
+  expect(calculate(5, 2, 2)).toBe(3);
+  expect(calculate(5, 3, 2)).toBe(10);
+  expect(calculate(5, 4, 2)).toBe(2.5);
+});
